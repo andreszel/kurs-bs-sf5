93b3dc188fc0e2c0d1831854b856aa3326bc3ce2
Andrzej Szelka <szelkaandrzej@gmail.com>
2023-10-23 18:07:21 +0200

    Finish homework - week 8

--- tests/LoginFormTest.php
+++ tests/LoginFormTest.php
@@ -8,42 +8,125 @@ class LoginFormTest extends WebTestCase
 {
     public function testLoginUserFormLogin(): void
     {
+        $email = 'webtestcase@kursbs.pl';
+        $password = 'test1234';
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->followRedirects();
+        $crawler = $client->request('GET', '/'); // Get homepage
 
-        $link = $crawler->selectLink('Login')->link();
-        $client->click($link);
+        $this->assertResponseIsSuccessful();
 
-        $csrfToken = static::getContainer()->get('security.csrf.token_manager')->getToken('division_item');
+        $loginLink = $crawler->selectLink('Login')->link();
+        $crawler = $client->click($loginLink);
+
+        //$csrfToken = static::getContainer()->get('security.csrf.token_manager')->getToken('division_item');
 
         // select the button
-        $buttonCrawlerNode = $crawler->selectButton('submit');
-        $form = $buttonCrawlerNode->form();
+        $authCrawlerNode = $crawler->selectButton('Zaloguj się'); // Get form button.
+        $form = $authCrawlerNode->form();
 
-        $form['email'] = 'webtestcase@kursbs.pl';
-        $form['password'] = 'test1234';
-        $form['_remember_me']->tick();
-        $form['_csrf_token'] = $csrfToken;
+        /* $form['email'] = $email;
+        $form['password'] = $password; */
+        //$form['_remember_me']->tick();
+
+        $form = $authCrawlerNode->form(array(
+            'email' => $email,
+            'password' => $password
+        ));
 
         $crawler = $client->submit($form);
 
-        $client->followRedirects(true);
-
         /* $crawler = $client->submit($form, [
-            'email' => 'webtestcase@kursbs.pl',
-            'password' => 'test1234'
+            'email' => $email,
+            'password' => $password
         ]); */
 
-        /* $crawler = $client->submitForm('Sign in', [
-            'email' => 'webtestcase@kursbs.pl',
-            'password' => 'test1234'
+        /* $client->submitForm($authCrawlerNode, [
+            'email' => $email,
+            'password' => $password,
+            //'_csrf_token' => $csrfToken
         ], 'POST'); */
 
         $link = $crawler->selectLink('Admin')->link();
         $client->click($link);
 
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
 
-        $this->assertResponseIsSuccessful();
+        //$this->assertResponseIsSuccessful();
         $this->assertSelectorTextContains('h1', 'Dashboard admin');
     }
+
+    public function test_user_can_view_a_login_form()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/login');
+
+        $response = $client->getResponse();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertResponseStatusCodeSame(200, $response->getStatusCode());
+    }
+
+    /* public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/home');
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        // Creamos un factory de user para que lo utilice en la prueba
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = '123456'),
+        ]);
+
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect('/home');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_cannot_login_with_incorrect_password()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('123456'),
+        ]);
+
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+
+    public function test_remember_me_functionality()
+    {
+        $user = factory(User::class)->create([
+            'id' => random_int(1, 100),
+            'password' => bcrypt($password = '123456'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+            'remember' => 'on',
+        ]);
+
+        $response->assertRedirect('/home');
+        // cookie assertion goes here
+        $this->assertAuthenticatedAs($user);
+    } */
 }
